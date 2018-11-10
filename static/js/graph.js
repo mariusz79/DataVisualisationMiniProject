8f72c799f7ada93887b4215431c6bd90ef8ec0fe
mariusz79 <mpawlowic@gmail.com>
2018-11-10 20:26:49 +0000

    Added Service Salary Correlation on Scatter Plot

--- static/js/graph.js
+++ static/js/graph.js
@@ -7,6 +7,7 @@ function makeGraphs(error, salaryData) {
 
     salaryData.forEach(function (d) {
         d.salary = parseInt(d.salary);
+        d.yrs_service = parseInt(d["yrs.service"]);
     })
 
     show_discipline_selector(ndx);          //pass 'ndx' variable to function that gonna draw a graph
@@ -17,6 +18,8 @@ function makeGraphs(error, salaryData) {
     show_gender_balance(ndx);
     show_average_salary(ndx);
     show_rank_distribution(ndx);
+
+    show_service_to_salary_correlation(ndx);
 
     dc.renderAll();                         //without it nothing will be show on the page
 }
@@ -178,4 +181,39 @@ function show_rank_distribution(ndx) {
         .xUnits(dc.units.ordinal)
         .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
         .margins({ top: 10, right: 100, bottom: 30, left: 30 });
+}
+
+function show_service_to_salary_correlation(ndx) {
+
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+
+    var eDim = ndx.dimension(dc.pluck("yrs_service"));
+    var experienceDim = ndx.dimension(function (d) {
+        return [d.yrs_service, d.salary, d.rank, d.sex];
+    });
+    var experienceSalaryGroup = experienceDim.group();
+
+    var minExperience = eDim.bottom(1)[0].yrs_service;
+    var maxExperience = eDim.top(1)[0].yrs_service;
+
+    dc.scatterPlot("#service-salary")
+        .width(800)
+        .height(400)
+        .x(d3.scale.linear().domain([minExperience, maxExperience]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .xAxisLabel("Years Of Service")
+        .title(function (d) {
+            return d.key[2] + " earned " + d.key[1];
+        })
+        .colorAccessor(function (d) {
+            return d.key[3];
+        })
+        .colors(genderColors)
+        .dimension(experienceDim)
+        .group(experienceSalaryGroup)
+        .margins({ top: 10, right: 50, bottom: 75, left: 75 });
 }
