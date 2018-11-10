380a02b5893ca82497b814d55c19dd94275f90b4
mariusz79 <mpawlowic@gmail.com>
2018-11-10 16:53:47 +0000

    updating salaries.csv

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,29 +1,30 @@
+//load data
 queue()
-    .defer(d3.csv, "data/Salaries.csv")
-    .await(makeGraphs);
+    .defer(d3.csv, "data/Salaries.csv")     // defer has 2 arguments; first is format of the data we want to load; second is the path to file
+    .await(makeGraphs);                     //await has one argument-the name of the function we want to call when the data is loaded
 
 function makeGraphs(error, salaryData) {
-    var ndx = crossfilter(salaryData);
+    var ndx = crossfilter(salaryData);      //create crossfilter
 
-    show_gender_balance(ndx);
+    show_gender_balance(ndx);               //pass 'ndx' variable to function that gonna draw a graph
 
-    dc.renderAll();
+    dc.renderAll();                         //without it nothing will be show on the page
 }
 
 function show_gender_balance(ndx) {
     var dim = ndx.dimension(dc.pluck('sex'));
     var group = dim.group();
 
-    dc.barChart("#gender-balance")
+    dc.barChart("#gender-balance")      //this barchart will be rendered in element with that id
         .width(400)
         .height(300)
         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
+        .transitionDuration(500)        //how quickly the chart animates when we filter
+        .x(d3.scale.ordinal())          //ordinal, beacause dimension consists of the words
         .xUnits(dc.units.ordinal)
         .elasticY(true)
-        .xAxisLabel("Gender")
-        .yAxis().ticks(20);
+        .xAxisLabel("Gender")           //label for x axis
+        .yAxis().ticks(20);             //number of ticks on y axis
 }
