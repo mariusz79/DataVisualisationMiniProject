db93408f9c37ccf443203a6fc1364dc209abd0be
mariusz79 <mpawlowic@gmail.com>
2018-11-10 16:18:55 +0000

    add Gender Balance Graph

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,9 +1,29 @@
-//load data
-
 queue()
-    .defer(d3.csv, 'data/Salaries.csv') //defer has 2 arguments; first is format of the data we want to load; second is the path to file
-    .await(makeGraphs);                //await has one argument-the name of the function we want to call when the data is loaded
+    .defer(d3.csv, "data/Salaries.csv")
+    .await(makeGraphs);
 
-function makeGraphs(error, salaryData){
-    
+function makeGraphs(error, salaryData) {
+    var ndx = crossfilter(salaryData);
+
+    show_gender_balance(ndx);
+
+    dc.renderAll();
+}
+
+function show_gender_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'));
+    var group = dim.group();
+
+    dc.barChart("#gender-balance")
+        .width(400)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
 }
